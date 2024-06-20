d6d483e7ac8ac6dcb4fde026a66d3b51a966a876
arthur <artyr007@gmail.com>
2024-06-20 21:12:46 +0500

    Add insert method for bst.js

--- bts.js
+++ bts.js
@@ -23,15 +23,40 @@ class Tree {
     treeNode.right = this.buildTree(array, rootIndex + 1, end);
     return treeNode;
   }
-  // prettier-ignore
-  prettyPrint(node = this.root, prefix = "", isLeft = true) {
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return;
+    }
+
+    let currentNode = this.root;
+    while (true) {
+      if (value < currentNode.data) {
+        if (currentNode.left === null) {
+          currentNode.left = newNode;
+          return;
+        }
+        currentNode = currentNode.left;
+      } else {
+        if (currentNode.right === null) {
+          currentNode.right = newNode;
+          return;
+        }
+        currentNode = currentNode.right;
+      }
+    }
+  }
+
+  printTree(node = this.root, prefix = "", isLeft = true) {
     if (node === null) return;
     if (node.right !== null) {
-      this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+      this.printTree(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
     }
     console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
     if (node.left !== null) {
-      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+      this.printTree(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
   }
 }
@@ -39,4 +64,7 @@ class Tree {
 const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 const myTree = new Tree(arr);
-myTree.prettyPrint();
+myTree.printTree();
+myTree.insert(40);
+myTree.insert(22);
+myTree.printTree();
