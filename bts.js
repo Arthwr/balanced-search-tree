e49d44c18da4311b4b9b3cc1a3c399064cfa79b3
arthur <artyr007@gmail.com>
2024-06-20 20:11:25 +0500

    Add buildTree function and prettyPrint function to visually represent BST

--- bts.js
+++ bts.js
@@ -1,17 +1,23 @@
 import quickSort from "./quickSort.js";
 import removeDuplicates from "./removeDuplicates.js";
+import prettyPrint from "./prettyPrint.js";
 
 let arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
-const node = (value = null, left = null, right = null) => {
-  return { value, left, right };
+const node = (data = null, left = null, right = null) => {
+  return { data, left, right };
 };
 
-const tree = () => {
-  let root = null;
-  return { root };
+const buildTree = (array, start = 0, end = array.length - 1) => {
+  if (start > end) return null;
+  const rootIndex = Math.floor((start + end) / 2);
+  const treeNode = node(array[rootIndex]);
+  treeNode.left = buildTree(array, start, rootIndex - 1);
+  treeNode.right = buildTree(array, rootIndex + 1, end);
+  return treeNode;
 };
 
-const buildTree = (array) => {
-  const uniqueSortedArr = removeDuplicates(quickSort(array));
-};
+const modifiedArray = quickSort(removeDuplicates(arr));
+const resultTree = buildTree(modifiedArray);
+console.log(resultTree);
+prettyPrint(resultTree);
