fd6078641d279d5d3b8e67f62fbb05427b839d8f
arthur <artyr007@gmail.com>
2024-06-20 20:27:39 +0500

    Refactor Node and Tree into class constructors && Split quickSort.js and removeDuplicates into helpers functions and into separate directory

--- bts.js
+++ bts.js
@@ -1,23 +1,42 @@
-import quickSort from "./quickSort.js";
-import removeDuplicates from "./removeDuplicates.js";
-import prettyPrint from "./prettyPrint.js";
+import quickSort from "./helpers/quickSort.js";
+import removeDuplicates from "./helpers/removeDuplicates.js";
 
-let arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+class Node {
+  constructor(data = null, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
 
-const node = (data = null, left = null, right = null) => {
-  return { data, left, right };
-};
+class Tree {
+  constructor(array) {
+    this.sortedArray = quickSort(removeDuplicates(array));
+    this.root = this.buildTree(this.sortedArray);
+  }
 
-const buildTree = (array, start = 0, end = array.length - 1) => {
-  if (start > end) return null;
-  const rootIndex = Math.floor((start + end) / 2);
-  const treeNode = node(array[rootIndex]);
-  treeNode.left = buildTree(array, start, rootIndex - 1);
-  treeNode.right = buildTree(array, rootIndex + 1, end);
-  return treeNode;
-};
+  buildTree(array, start = 0, end = array.length - 1) {
+    if (start > end) return null;
+    const rootIndex = Math.floor((start + end) / 2);
+    const treeNode = new Node(array[rootIndex]);
+    treeNode.left = this.buildTree(array, start, rootIndex - 1);
+    treeNode.right = this.buildTree(array, rootIndex + 1, end);
+    return treeNode;
+  }
+  // prettier-ignore
+  prettyPrint(node = this.root, prefix = "", isLeft = true) {
+    if (node === null) return;
+    if (node.right !== null) {
+      this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  }
+}
 
-const modifiedArray = quickSort(removeDuplicates(arr));
-const resultTree = buildTree(modifiedArray);
-console.log(resultTree);
-prettyPrint(resultTree);
+const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+const myTree = new Tree(arr);
+myTree.prettyPrint();
